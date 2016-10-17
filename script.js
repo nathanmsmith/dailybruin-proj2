46a75e755bddae969556e74384dc0ccb85bf8398
Nathan Smith <nathan@smith.industries>
2016-10-17 00:01:11 -0700

    Add geolocation

--- script.js
+++ script.js
@@ -3,5 +3,18 @@ $(() => {
     event.preventDefault()
     const location = $("input:first").val()
     alert("Sumbitted! Location: " + location)
-  });
-});
+  })
+
+  $(".geolocation-search").click(event => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        let pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        }
+
+        alert("position: " + pos)
+      })
+    }
+  })
+})
