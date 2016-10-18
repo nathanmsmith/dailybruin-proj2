e636f1e5e92fb5c94dd03cb68557e478d429eaf9
Nathan Smith <nathan@smith.industries>
2016-10-18 15:21:39 -0700

    Update to Airbnb JS style

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 $(() => {
-  $(".location-search").submit(event => {
-    event.preventDefault()
-    const location = $("input:first").val()
-    alert("Sumbitted! Location: " + location)
-  })
+  $('.location-search').submit((event) => {
+    event.preventDefault();
+    const location = $('input:first').val();
+    alert(`Sumbitted! Location: ${location}`);
+  });
 
-  $(".geolocation-search").click(event => {
+  $('.geolocation-search').click(() => {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(position => {
-        let pos = {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const pos = {
           lat: position.coords.latitude,
-          lng: position.coords.longitude
-        }
+          lng: position.coords.longitude,
+        };
 
-        alert("position: " + pos)
-      })
+        alert(`position: ${pos}`);
+      });
     }
-  })
-})
+  });
+});
