530dfe30a300c98fb38cec9ba509ed7516543a16
Xinyu Yang <colin.yangxinyu@outlook.com>
2016-10-22 23:00:34 -0700

    prepare google map

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-$(() => {
+/* global $ google */
+
+$(document).ready(() => {
   $('.location-search').submit((event) => {
     event.preventDefault();
     const location = $('input:first').val();
@@ -10,7 +12,7 @@ $(() => {
       navigator.geolocation.getCurrentPosition((position) => {
         const pos = {
           lat: position.coords.latitude,
-          lng: position.coords.longitude,
+          lng: position.coords.longitude
         };
 
         alert(`position: ${pos}`);
@@ -18,3 +20,12 @@ $(() => {
     }
   });
 });
+
+
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    scrollwheel: false,
+    zoom: 8
+  });
+}
